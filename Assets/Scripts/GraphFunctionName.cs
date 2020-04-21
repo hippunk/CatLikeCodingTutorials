c8977fd90bc83ae5dd878e74048e35c7ab28f16f
Hipp <arthur.ramolet@hotmail.fr>
2020-04-21 15:52:35 +0200

    GraphVisualizer

---
+++ Assets/Scripts/GraphFunctionName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GraphFunctionName
+{
+    Empty,
+    Sine,
+    Sine2D,
+    MultiSine,
+    MultiSine2DFunction,
+    Ripple,
+    Cylinder,
+    Sphere,
+    Torus
+}
