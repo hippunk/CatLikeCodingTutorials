d0b7d2ae18b9a39fa6b84c81ade47052307e8df7
Hipp <arthur.ramolet@hotmail.fr>
2020-05-23 16:40:43 +0200

    Mesh deformer

---
+++ Assets/Scripts/Mesh Basic/CountVertices.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountVertices : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Debug.Log("Vertices count : "+mesh.vertices.Length);
+    }
+
+    
+}
